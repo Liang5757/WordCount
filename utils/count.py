a4fb93f6609bb77186ec26901b74d73dede4c6b4
w1036933220 <45329976+w1036933220@users.noreply.github.com>
2020-03-21 23:09:55 +0800

    添加了计算（代码行/空行/注释行）的功能

--- utils/count.py
+++ utils/count.py
@@ -10,11 +10,11 @@ class FileProperties(object):
         # 单词数
         self.word_num = 0
         # 行数
-        self.line_num = 0
-        # 代码行
-        self.code_line_num = 0
+        self.line_num = len(file_text)
         # 空行
         self.null_line_num = 0
+        # 代码行
+        self.code_line_num = 0
         # 注释行数
         self.annotation_line_num = 0
 
@@ -27,15 +27,47 @@ class FileProperties(object):
 
     # 计算单词数
     def count_word_num(self):
-        word_num = 0
         for line in self.file_text:
             # 正则匹配一行中的所有单词，并计算单词数
-            word_num += len(re.findall(r'[a-zA-Z]+[\-\']?[a-zA-Z]*', line))
+            self.word_num += len(re.findall(r'[a-zA-Z0-9]+[\-\']?[a-zA-Z]*', line))
 
-        return word_num
+        return self.word_num
 
     # 计算行数
     def count_line_num(self):
-        self.line_num = len(self.file_text)
 
         return self.line_num
+
+    # 计算空行数
+    def count_null_line_num(self):
+        for line in self.file_text:
+            # 只有不超过一个可显示的字符
+            if len(re.findall(r'\S', line)) <= 1:
+                self.null_line_num += 1
+
+        return self.null_line_num
+
+    # 计算代码行
+    def count_code_line_num(self):
+        return self.line_num - self.null_line_num - self.annotation_line_num
+
+    def count_annotation_line_num(self):
+        flag = 0
+        for line in self.file_text:
+            line = line.strip()
+            # 匹配不是代码行且有//
+            if re.match(r'^\S?\s*?\/\/', line):
+                self.annotation_line_num += 1
+            # 匹配不是代码行且有/*
+            elif re.match(r'^\S?\s*?\/\*', line):
+                flag = 1
+                self.annotation_line_num += 1
+                if line.endswith('*/'):
+                    flag = 0
+            elif flag == 1:
+                self.annotation_line_num += 1
+            elif "*/" in line:
+                self.annotation_line_num += 1
+                flag = 0
+
+        return self.annotation_line_num
--- utils/orders.py
+++ utils/orders.py
@@ -3,7 +3,7 @@ from utils.count import *
 
 # 输出字符数
 def print_char_num(text):
-    print(FileProperties(text).count_char_num())
+    print("字符数" + FileProperties(text).count_char_num())
 
 
 # 输出单词数
@@ -16,6 +16,15 @@ def print_line_num(text):
     print(FileProperties(text).count_line_num())
 
 
+# 输出代码行/空行/注释行
+def print_code_property(text):
+    file_properties = FileProperties(text)
+
+    print("空行：" + str(file_properties.count_null_line_num()))
+    print("注释行：" + str(file_properties.count_annotation_line_num()))
+    print("代码行：" + str(file_properties.count_code_line_num()))
+
+
 # 命令集
 orders = {
     '-c': print_char_num,
@@ -24,9 +33,9 @@ orders = {
 
     "-l": print_line_num,
 
-    '-s': print("递归调用"),
+    "-a": print_code_property,
 
-    "-a": print("代码行/空行/注释行"),
+    '-s': print("递归调用"),
 
     "-x": print("图像界面")
 }
